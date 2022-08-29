99694a929e64cb266641e915c3841c527fe7791f
abtullohn <abtullohn@gmail.com>
2022-08-29 15:40:12 -0400

    database

--- models/Seed.js
+++ models/Seed.js
@@ -4,13 +4,29 @@ const { Comment, Image, User } = require('./');
 
 const images = [
     {
-        url: 'https://dummyimage.com/600x400/ff05ff/9ea0b8.jpg',
+        url: 'https://dummyimage.com/600x400/005c66/fff',
         description: 'this is a test',
         likes: 10000000000,
+        user: '630d14bde0d3cb80372ea249'
     }
 ]
 
-//Image.insertMany(images)
+const comments = [
+    {
+        content: 'that was bomb',
+        image: '630d0b4fb9dae18b0b7422b2',
+        user: '630d14bde0d3cb80372ea249'
+    }
+]
 
+const users = [
+    {
+        username:'JoeyC',
+        password: 'myboobs'
 
-User.insertMany(user1)
+    }
+]
+
+Image.insertMany(images)
+Comment.insertMany(comments)
+User.insertMany(users)
