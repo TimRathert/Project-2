dfada2959086bb0f8cdf49ff13d3026b83b05dfa
TimRathert <rathertt@gmail.com>
2022-08-30 10:58:27 -0400

    working on new comment route and form

--- controllers/commentController.js
+++ controllers/commentController.js
@@ -10,12 +10,31 @@ router.use(express.urlencoded({ extended: false }));
 // ROUTES
 
 // get all comments
-router.get('/:id/', async (req, res, next) => {
-
-
-})
+router.get('/', async (req, res, next) => {
+    try {
+        const allComments = await db.Comment.find().populate('image').exec()
+        const allImages = await db.Image.find({})
+        res.render('pages/comments/index', {comments: allComments, images: allImages})
+    }
+    catch (err) {
+        console.log(err);
+        req.error = err;
+        return next();
+    }
+});
 // new comment
-
+router.post('/', async (req, res, next) => {
+    try{
+        const newComment = await db.Comment.create(req.body)
+        console.log(newComment)
+        res.redirect(`/images/${newComment.image}`)
+    }
+    catch (err) {
+        console.log(err);
+        req.error = err;
+        return next();
+    }
+})
 // show specific
 
 // edit comment
